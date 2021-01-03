819c9fd9da23de278598ec38addfaada0ace557b
kunal <kunalc3626@gmail.com>
2021-01-04 00:51:43 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'horoscope',
+    loadChildren: () => import('./horoscope/horoscope.module').then( m => m.HoroscopePageModule)
+  },
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ModalController } from '@ionic/angular';
+import { HoroscopePage } from '../horoscope/horoscope.page';
+import {Plugins, LocalNotification, LocalNotificationAction, LocalNotificationActionPerformed, LocalNotificationSchedule, Device, LocalNotificationEnabledResult, LocalNotifications} from '@capacitor/core';
+const {LocalNotification} = Plugins;
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  horoscopes:string[]; 
+  day: string;
+  sign: string;
 
-  constructor() {}
+  constructor(private http: HttpClient, public modalController: ModalController) {}
+
+  async ngOnInit(){
+    this.day = "today";
+  }
+
+  segmentChanged(ev: any){
+    console.log('segment changed', ev.detail.value);
+    this.day = ev.detail.value;
+  }
+
+  async showModel(sign,img){
+    const modal = await this.modalController.create({
+      component: HoroscopePage,
+      componentProps: {
+        day: this.day,
+        sign: sign,
+        img: img
+      }
+    });
+    return await modal.present();
+  }
 
 }
